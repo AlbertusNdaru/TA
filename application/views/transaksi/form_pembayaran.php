c3dd9a85161695e3078925cdf98b120706297894
Sinta Rostika <sintarostika28@gmail.com>
2019-02-23 07:43:13 +0700

    TA

--- application/views/transaksi/form_pembayaran.php
+++ application/views/transaksi/form_pembayaran.php
@@ -29,6 +29,15 @@
                                     <label>Upload Foto</label>
                                     <input required id="inputfoto" accept="image/x-png,image/gif,image/jpeg" type="file" class="form-control" name="berkas" placeholder="upload">
                                 </div>
+                                <div class="form-group">
+                                    <label>Jumlah Uang</label>
+                                    <input  id="jmlinput" class="form-control" name="inputuang" placeholder="jumlah uang" required >
+                                </div>
+                                <div class="form-group">
+                                    <label>Total</label>
+                                    <input  id="total" class="form-control" name="total" placeholder="total bayar" required readonly>
+                                    <input hidden id="jmltotal" class="form-control" name="total" placeholder="total bayar" required readonly>
+                                </div>
                                 <button type="submit" id="btnsimpanbarang" name="submit" class="btn btn-primary btn-sm" >Simpan</button> | 
                                 <?php echo anchor('penjualan','Kembali',array('class'=>'btn btn-danger btn-sm'))?>
                            </form>
@@ -57,3 +66,5 @@ function pilihtransaksi() {
     })
 }
 </script>
+
+
